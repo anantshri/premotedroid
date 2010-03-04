53310d7163b8d0d48fabb7e6a75d0c415f864d55
pierredurand <pierredurand@913ef97e-1a2b-11df-85c2-67338153f9dd>
2010-03-04 17:06:25 +0000

    view holder and adapter for ConnectionListActivity
    git-svn-id: http://premotedroid.googlecode.com/svn/trunk@159 913ef97e-1a2b-11df-85c2-67338153f9dd

---
+++ PRemoteDroid Client/src/org/pierre/remotedroid/client/activity/ConnectionEditActivity.java
@@ -0,0 +1,8 @@
+package org.pierre.remotedroid.client.activity;
+
+import android.app.Activity;
+
+public class ConnectionEditActivity extends Activity
+{
+	
+}
---
+++ PRemoteDroid Client/src/org/pierre/remotedroid/client/activity/ConnectionListActivity.java
@@ -0,0 +1,124 @@
+package org.pierre.remotedroid.client.activity;
+
+import org.pierre.remotedroid.client.R;
+import org.pierre.remotedroid.client.connection.Connection;
+import org.pierre.remotedroid.client.connection.ConnectionBluetooth;
+import org.pierre.remotedroid.client.connection.ConnectionList;
+import org.pierre.remotedroid.client.connection.ConnectionWifi;
+
+import android.app.ListActivity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class ConnectionListActivity extends ListActivity
+{
+	private class ConnectionListAdapter extends BaseAdapter
+	{
+		private ConnectionList connections;
+		private LayoutInflater layoutInflater;
+		
+		public ConnectionListAdapter(Context context, ConnectionList connections)
+		{
+			this.connections = connections;
+			
+			this.layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		}
+		
+		public int getItemViewType(int position)
+		{
+			Connection connection = this.connections.get(position);
+			
+			if (connection instanceof ConnectionWifi)
+			{
+				return 0;
+			}
+			else if (connection instanceof ConnectionBluetooth)
+			{
+				return 1;
+			}
+			
+			return IGNORE_ITEM_VIEW_TYPE;
+		}
+		
+		public int getViewTypeCount()
+		{
+			return 0;
+		}
+		
+		public int getCount()
+		{
+			return this.connections.getCount();
+		}
+		
+		public Object getItem(int position)
+		{
+			return this.connections.get(position);
+		}
+		
+		public long getItemId(int position)
+		{
+			return position;
+		}
+		
+		public View getView(int position, View convertView, ViewGroup parent)
+		{
+			ConnectionViewHolder holder;
+			
+			Connection connection = this.connections.get(position);
+			
+			if (convertView == null)
+			{
+				holder = new ConnectionViewHolder();
+				
+				if (connection instanceof ConnectionWifi)
+				{
+					convertView = this.layoutInflater.inflate(R.layout.connectionwifi, null);
+					
+					holder.address = (TextView) convertView.findViewById(R.id.hostPort);
+				}
+				else if (connection instanceof ConnectionBluetooth)
+				{
+					convertView = this.layoutInflater.inflate(R.layout.connectionbluetooth, null);
+					
+					holder.address = (TextView) convertView.findViewById(R.id.address);
+				}
+				
+				holder.name = (TextView) convertView.findViewById(R.id.name);
+				
+				convertView.setTag(holder);
+			}
+			else
+			{
+				holder = (ConnectionViewHolder) convertView.getTag();
+			}
+			
+			holder.name.setText(connection.getName());
+			
+			if (connection instanceof ConnectionWifi)
+			{
+				ConnectionWifi connectionWifi = (ConnectionWifi) connection;
+				
+				holder.hostPort.setText(connectionWifi.getHost() + ":" + connectionWifi.getPort());
+			}
+			else if (connection instanceof ConnectionBluetooth)
+			{
+				ConnectionBluetooth connectionBluetooth = (ConnectionBluetooth) connection;
+				
+				holder.address.setText(connectionBluetooth.getAddress());
+			}
+			
+			return convertView;
+		}
+	}
+	
+	private class ConnectionViewHolder
+	{
+		public TextView name;
+		public TextView hostPort;
+		public TextView address;
+	}
+}
